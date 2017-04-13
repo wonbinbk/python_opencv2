867d20537def730aa6c8ec3db70c1f2f26fb9586
Thai <thaiphd@gmail.com>
2017-04-13 14:44:16 +0700

    define new class ROI

--- ROI.py
+++ ROI.py
@@ -12,77 +12,99 @@ import cv2
 import sys
 
 
-def draw_rect(event, x, y, flags, (img, windows_name)):
-    global ix, iy
-    global drawing
-    img_display = img
-    temp_img = img_display.copy()
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if mode == 'anchors':
-            drawing = False
-            anchors.append((x, y))
-        elif mode == 'rectangles':
-            drawing = True
-            ix, iy = x, y
-            rectangles.append([(ix, iy), None])
-    elif (event == cv2.EVENT_MOUSEMOVE) & drawing:
-        # TODO
-        rectangles.pop()
-        temp_img = img_display.copy()
-        if mode == 'rectangles':
-            cv2.rectangle(temp_img, (ix, iy), (x, y), (0, 255, 0), 2)
-        cv2.imshow(windows_name, temp_img)
-        rectangles.append([(ix, iy), (x, y)])
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-    img_display = temp_img.copy()
+class ROI:
+    COLOR_RECTANGLE = (0, 255, 0)
+    COLOR_ANCHOR = (0, 255, 255)
+    REC_THICKNESS = 8
+    ANC_THICKNESS = 3
+    ANCHOR_SIZE = 20
+
+    def __init__(self):
+        self.anchors = []
+        self.rectangles = []
+        self.mode = 0
+        self.drawing = 0
+        self.ix, self.iy = -1, -1
+        self.leftclicked = 0
+        self.img = None
+
+    def onMouse(self, event, x, y, flags, param):
+        '''
+        Mouse events' callback
+        '''
+        if self.mode:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self.anchors.append((x, y))
+        elif event == cv2.EVENT_LBUTTONDOWN:
+            self.ix, self.iy = x, y
+            self.leftclicked = 1
+            self.rectangles.append([None, None])
+        elif event == cv2.EVENT_MOUSEMOVE and self.leftclicked:
+            self.drawing = 1
+            self.rectangles.pop()
+            self.rectangles.append([(self.ix, self.iy), (x, y)])
+        elif event == cv2.EVENT_LBUTTONUP:
+            if not self.drawing:
+                self.rectangles.pop()
+            else:
+                self.drawing = 0
+            self.leftclicked = 0
+
+    def drawing_anchors_and_rects(self, img):
+        '''
+        Draw rectangles and points (anchors) on an input image
+        Return: output image with rectangles and points
+        '''
+        self.img = img.copy()
+        for rect in self.rectangles:
+            cv2.rectangle(
+                self.img,
+                rect[0],
+                rect[1],
+                ROI.COLOR_RECTANGLE,
+                ROI.REC_THICKNESS)
+        for anchor in self.anchors:
+            cv2.circle(
+                self.img,
+                anchor,
+                ROI.ANCHOR_SIZE,
+                ROI.COLOR_ANCHOR,
+                ROI.ANC_THICKNESS)
+            cv2.line(
+                self.img,
+                (anchor[0] - 2 * ROI.ANCHOR_SIZE, anchor[1]),
+                (anchor[0] + 2 * ROI.ANCHOR_SIZE, anchor[1]),
+                ROI.COLOR_ANCHOR,
+                ROI.ANC_THICKNESS)
+            cv2.line(
+                self.img,
+                (anchor[0], anchor[1] - 2 * ROI.ANCHOR_SIZE),
+                (anchor[0], anchor[1] + 2 * ROI.ANCHOR_SIZE),
+                ROI.COLOR_ANCHOR,
+                ROI.ANC_THICKNESS)
+
+        return self.img
 
 
 if __name__ == '__main__':
-    point = (20, 50)    # point[0] = point radius; point[1] = line length
-    rectangles = []
-    anchors = []
-    ix, iy = -1, -1
-    drawing = False
-    mode = 'rectangles'
+    myROI = ROI()
     img = cv2.imread(sys.argv[1], 1)
-    img_display = img.copy()
-    windows_name = 'image'
-    cv2.namedWindow(windows_name, cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback(windows_name, draw_rect, (img, windows_name))
-    cv2.imshow(windows_name, img_display)
-
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback('image', myROI.onMouse)
+    cv2.imshow('image', img)
     while(True):
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('z'):
-            if (mode == 'rectangles') & (len(rectangles) > 0):
-                rectangles.pop()
-            elif (mode == 'anchors') & (len(anchors) > 0):
-                anchors.pop()
-        elif key == ord('a'):
-            mode = 'anchors'
+        if key == ord('a'):
+            myROI.mode = 1
         elif key == ord('r'):
-            mode = 'rectangles'
+            myROI.mode = 0
+        elif key == ord('z'):
+            if myROI.rectangles and not myROI.mode:
+                myROI.rectangles.pop()
+            elif myROI.anchors and myROI.mode:
+                myROI.anchors.pop()
         elif key == ord('q'):
             break
-        img_display = img.copy()
-        for rect in rectangles:
-            if rect[1]:
-                cv2.rectangle(img_display, rect[0], rect[1], (0, 255, 0), 2)
-        for anchor in anchors:
-            cv2.circle(img_display,
-                       (anchor[0], anchor[1]),
-                       point[0], (0, 255, 255), 2)
-            cv2.line(img_display,
-                     (anchor[0] - point[1]/2, anchor[1]),
-                     (anchor[0] + point[1]/2, anchor[1]),
-                     (0, 255, 255), 2)
-            cv2.line(img_display,
-                     (anchor[0], anchor[1] - point[1]/2),
-                     (anchor[0], anchor[1] + point[1]/2),
-                     (0, 255, 255), 2)
-        cv2.imshow(windows_name, img_display)
-
-    print "List of anchor points:\n"
-    print "".join(str(anchor for anchor in anchors))
-    cv2.destroyAllWindows()
+        myROI.drawing_anchors_and_rects(img)
+        cv2.imshow('image', myROI.img)
+    cv2.destroyWindow('image')
